e426bc892722ddd60507de668ad8f6af6cd5af8a
Harsha Gelli <hgelli@osius.com>
2025-09-13 22:25:16 +0530

    fix for the file paths for build paths 3

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
-import useLogout from "../Utils/UseLogout"; 
+import useLogout from "../Utils/useLogout"; 
 
 const NavBar = ()=> {
   const user = useSelector((state)=> state.user);
